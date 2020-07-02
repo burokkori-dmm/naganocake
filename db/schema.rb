422ab0f75ceaf2e389b9bfb88c3bfa1bd5171d2c
vagrant <vagrant@localhost.localdomain>
2020-07-02 10:20:53 +0000

    [Add]sweet/genreモデル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_02_090621) do
+ActiveRecord::Schema.define(version: 2020_07_02_093918) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +22,24 @@ ActiveRecord::Schema.define(version: 2020_07_02_090621) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.boolean "genre_status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sweets", force: :cascade do |t|
+    t.integer "genre_id"
+    t.string "name"
+    t.integer "tax_free_price"
+    t.text "detail"
+    t.string "image_id"
+    t.integer "sale_status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
