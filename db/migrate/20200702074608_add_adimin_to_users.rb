22ef359f9772791e658d45b036916a8b09c45ba8
kazuma012 <63410320+kazuma012@users.noreply.github.com>
2020-07-02 17:13:01 +0900

    Update 20200702074608_add_adimin_to_users.rb
    大変申し訳ありません🙇

--- db/migrate/20200702074608_add_adimin_to_users.rb
+++ db/migrate/20200702074608_add_adimin_to_users.rb
@@ -1,4 +1,4 @@
-class AddAdiminToUsers < ActiveRecord::Migration[5.2]
+class AddAdminToUsers < ActiveRecord::Migration[5.2]
   def change
   	add_column :users, :admin, :boolean, default: false
   end
