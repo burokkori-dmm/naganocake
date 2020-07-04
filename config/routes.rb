3db7f14a09696a96b2ce83dcf02a575b8ef0ba15
unknown <hannzaimonn@gmail.com>
2020-07-04 14:05:28 +0900

    order,controllre

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'orders/new'
+    get 'orders/show'
+    get 'orders/create'
+    get 'orders/success'
+  end
+  namespace :admin do
+    get 'order_details/update'
+  end
+  namespace :admin do
+    get 'order/index'
+    get 'order/show'
+    get 'order/update'
+  end
   devise_for :admins
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
