23faf38d41afcd33792739249fa748ed3c5ffa60
vagrant <vagrant@localhost.localdomain>
2020-07-04 05:25:19 +0000

    コントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'users/new'
+  end
+  namespace :admin do
+    get 'admins/new'
+  end
+  namespace :admin do
+    get 'users/new'
+  end
   devise_for :admins
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
