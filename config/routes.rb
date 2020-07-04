5c3a0a7881dac782f3c13e6d073082e43a721dc6
vagrant <vagrant@localhost.localdomain>
2020-07-04 06:31:06 +0000

    root設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,42 +1,21 @@
 Rails.application.routes.draw do
+  root 'public/home#top'
   namespace :public do
+    devise_for :users
     get 'home/about'
-    get 'home/top'
-  end
-  namespace :public do
+
     resources :deliverys
-  end
-  namespace :public do
+
     resources :carts_items
-  ebd
-  namespace :admin do  
-    get 'users/new'
-  end
-  namespace :admin do
-    get 'admins/new'
-  end
-  namespace :admin do
-    get 'users/new'
-  end
-  namespace :public do
     get 'sweets/index'
     get 'sweets/show'
   end
+
   namespace :admin do
-    get 'genres/index'
-    get 'genres/create'
-    get 'genres/edit'
-    get 'genres/update'
+    devise_for :admins
+    resources :genres
+    resources :sweets
+    resources :users
   end
-  namespace :admin do
-    get 'sweets/index'
-    get 'sweets/new'
-    get 'sweets/create'
-    get 'sweets/show'
-    get 'sweets/edit'
-    get 'sweets/update'
-  end
-  devise_for :admins
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
