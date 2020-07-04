2afb6d374f759867a2e4b1b38ff102a33f4360c4
vagrant <vagrant@localhost.localdomain>
2020-07-04 05:24:20 +0000

    [Add]controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   namespace :public do
+    get 'home/about'
+    get 'home/top'
+  end
+  namespace :public do
     resources :deliverys
   end
   namespace :public do
