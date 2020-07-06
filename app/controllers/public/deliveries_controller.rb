fa9f25e041e321a0fa874222d5570da8575eee68
vagrant <vagrant@localhost.localdomain>
2020-07-06 04:27:04 +0000

    [Rev]

--- app/controllers/public/deliveries_controller.rb
+++ app/controllers/public/deliveries_controller.rb
@@ -1,5 +1,7 @@
 class Public::DeliveriesController < ApplicationController
 
+before_action :setup_delivery, only: [:edit, :update, :destroy]
+
   def index
     #一覧表示(each),新規投稿(form_for)
     @delivery = Delivery.new
@@ -10,33 +12,38 @@ class Public::DeliveriesController < ApplicationController
     #新規投稿の追加
     @delivery = Delivery.new(delivery_params)
     if @delivery.save
-    redirect to public_deliveries_path
+    redirect_to public_deliveries_path
     else
     render "index"
     end
   end
 
   def edit
-    #編集ページ
-    @delivery = Delivery.find(params[:id])
+    #編集ページ(setup_deliveries使用)
   end
 
   def update
-    #編集ページの更新
-    @delivery = Delivery.find(params[:id])
+    #編集更新(setup_deliveries使用)
     if @delivery.update(delivery_params)
-    redirect to public_deliveries_path
+    redirect_to public_deliveries_path
     else
     render "edit"
     end
   end
 
   def destroy
-    #情報削除
+    #情報削除(setup_deliveries使用)
+    @delivery.destroy
+    redirect_to public_deliveries_path
   end
 
 private
   def delivery_params
     params.require(:delivery).permit(:postal_cade, :address, :name)
   end
+
+  def setup_delivery
+    @delivery = Delivery.find(params[:id])
+
+
 end
