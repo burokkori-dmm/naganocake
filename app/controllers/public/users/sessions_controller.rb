074669b39b88272f602655d5089ac8a8ac2f6e6a
vagrant <vagrant@localhost.localdomain>
2020-07-09 07:57:11 +0000

    [rev]

--- app/controllers/public/users/sessions_controller.rb
+++ app/controllers/public/users/sessions_controller.rb
@@ -25,15 +25,16 @@ class Public::Users::SessionsController < Devise::SessionsController
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
 
-  #def reject_user
-    #@user = User.find_by(email: params[:user][:email].downcase)
-    #if @user
-      #if (@user.valid_password?(params[:user][:password]) && (@user.active_for_authentication? == false))
-        #flash[:error] = "退会済みです。"
-        #redirect_to new_user_session_path
-      #end
-    #else
-      #flash[:error] = "必須項目を入力してください。"
-    #end
-  #end
+
+  # def reject_user
+  #   @user = User.find_by(email: params[:user][:email].downcase)
+  #   if @user
+  #     if (@user.valid_password?(params[:user][:password]) && (@user.active_for_authentication? == false))
+  #       flash[:error] = "退会済みです。"
+  #       redirect_to new_user_session_path
+  #     end
+  #   else
+  #     flash[:error] = "必須項目を入力してください。"
+  #   end
+  # end
 end
