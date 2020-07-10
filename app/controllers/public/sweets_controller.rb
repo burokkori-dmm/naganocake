4801c8d543aa64f7e2011c8e8287ac1edd80cf6b
vagrant <vagrant@localhost.localdomain>
2020-07-10 09:26:16 +0000

    [Add]ジャンル検索

--- app/controllers/public/sweets_controller.rb
+++ app/controllers/public/sweets_controller.rb
@@ -10,6 +10,18 @@ class Public::SweetsController < ApplicationController
   	@cart_item = CartItem.new
   end
 
+  def new
+    # 例えば↓の@sweetsにジャンルid１のsweetを持ってきたい
+    # @sweets = Sweet.find_by(name)
+    # @genres = Genre.all
+  end
+
+
+  def search
+    @sweets = Sweet.all
+  end
+
+
   # ↓adminでは必要でpublicでは不要？何故？ちなみにこれがあるとshowの表示の際にエラーが出る↓
   # private
   # def sweet_params
