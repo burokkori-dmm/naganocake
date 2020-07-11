d77e6ca67a80b149b65d720b02beaab8413b7fe6
vagrant <vagrant@localhost.localdomain>
2020-07-11 06:48:23 +0000

    [Rev]

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,27 +1,24 @@
 class Public::CartItemsController < ApplicationController
 
-  before_action :setup_cart_item!, only: [:update, :destroy]
+  #before_action :setup_cart_item!, only: [:update, :destroy]
 
   def index
     #カート内商品を表示させる
     #@cart_items = CartItem.where(user_id: current_user.id)
-    cart_items = CartItem.all
     @cart_items = current_user.cart_items.page(params[:page]).reverse_order
+    #合計金額
     @total_price = 0
-    #合計価格
 
   end
 
   def create
-    @cart_item = current_user.cart_items.find_by(sweet_id: params[:sweet_id])
+       @cart_item = current_user.cart_items.find_by(sweet_id: params[:sweet_id])
     if @cart_item.blank?
-    @cart_item = CartItem.new(cart_item_params)
-    @cart_item.user_id = current_user.id
-    @cart_item.peace += params[:peace].to_i
-    @cart_item.save
-    redirect_to public_cart_items_path, notice: '商品をカートに追加しました！'
-    else
-    render 'index'
+       @cart_item = CartItem.new(cart_item_params)
+       @cart_item.user_id = current_user.id
+       @cart_item.peace += params[:peace].to_i
+       @cart_item.save
+       redirect_to public_cart_items_path, notice: '商品をカートに追加しました！'
     end
   end
 
@@ -41,8 +38,8 @@ class Public::CartItemsController < ApplicationController
 
   def destroy_all
     #全て削除(余裕があれば非同期)
-    @user = current_user
-    @user.destroy
+    @cart_items = current_user.cart_items
+    @cart_items.destroy_all
     redirect_to public_cart_items_path
   end
 
@@ -51,8 +48,8 @@ protected
     params.require(:cart_item).permit(:sweet_id, :peace, :user_id)
   end
 
-  def setup_cart_item!
-    cart_item = current_user.cart_items.find(params[:id])
-  end
+  # def setup_cart_item!
+  #   cart_item = current_user.cart_items.find(params[:id])
+  # end
 
 end
